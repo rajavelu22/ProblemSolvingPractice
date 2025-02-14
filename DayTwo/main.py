a7ea51d3bc99a28b87ad64aee916599d8af63c7a
rajavelu22 <rajavelujansi@gmail.com>
2025-02-14 07:00:45 +0530

    Day Two 14-02-2025 7:00 A.M.

--- DayTwo/main.py
+++ DayTwo/main.py
@@ -61,3 +61,51 @@ for D in ToDict :
     X.append( str(round(math.sqrt(D*C/H))) )
     
 print(X)
+
+"""Question 7
+Level 2
+
+Question:
+Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array. The element value in the i-th row and j-th column of the array should be i*j.
+Note: i=0,1.., X-1; j=0,1,¡­Y-1.
+Example
+Suppose the following inputs are given to the program:
+3,5
+Then, the output of the program should be:
+[[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]] 
+
+Hints:
+Note: In case of input data being supplied to the question, it should be assumed to be a console input in a comma-separated form."""
+
+Getting the input from the user 
+X = int(input("Enter the number of rows "))
+Y = int(input("Enter the number of columns "))
+
+List = []
+
+for i in range (0,X) :
+    New_List=[]
+    for j in range (0,Y) :
+        value = input(f" Enter the value for the index {i} {j} ")
+        New_List.append(value)
+        List.append(New_List)
+        
+print(List)
+
+
+"""Question 8
+Level 2
+
+Question:
+Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+Suppose the following input is supplied to the program:
+without,hello,bag,world
+Then, the output should be:
+bag,hello,without,world
+
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input."""
+
+List = [x for x in input().split(',')]
+List.sort()
+print (",".join(List))
